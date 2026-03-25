500de9e9558d9fb1b287c5c4fab9b9d9476c052a
Franco Bocanegra <franco.bocanegra@outlook.com>
2026-03-25 20:07:46 -0300

    clase de practica 5

--- Main.cpp
+++ Main.cpp
@@ -1,63 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 
-int edad = 20;
-float altura = 1.75;
-double peso = 70.345678;
-char inicial = 'A';
-long long distancia = 1234567890;
-int n = 42;
-
-int edad2 = 0;
-
 int main() {
-printf("Edad: %d\n", edad);
-printf("Altura: %f\n", altura);
-printf("Peso: %lf\n", peso);
-printf("Inicial: %c\n", inicial);
-printf("Distancia: %lld\n", distancia);
+int a, b, c;
 
+printf("Coloca un valor entero : ");
+scanf("%d", &a);
+printf("Coloca otro segundo valor entero : ");
+scanf("%d", &b);
+printf("Coloca otro tercer valor entero : ");
+scanf("%d", &c);
 
-printf("%d \n", n);
-printf("%5d \n", n);
-printf("%10d \n", n);
+printf("los valores son: %d %d %d\n" , a , b , c);
 
-int edad;
-printf("Ingrese edad: ");
-scanf("%d", &edad);
-float altura;
-printf("Ingrese altura: ");
-scanf("%f", &altura);
-double peso;
-printf("Ingrese peso: ");
-scanf("%lf", &peso);
-char inicial;
-printf("Ingrese una letra: ");
-scanf(" %c", &inicial);
-
-printf("Edad: %d\n", edad);
-printf("Altura: %f\n", altura);
-printf("Peso: %lf\n", peso);
-printf("Inicial: %c\n", inicial);
-
-printf("Edad Dos: " , edad2);
-scanf("%d", &edad2);
-printf("la edad dos es : %d\n", edad2);
-printf("La suma de las edades es %d\n", edad + edad2);
-
-int numero;
-printf("Ingrese numero: ");
-scanf("%d", &numero);
-printf("Mayor que 10: %d\n", numero > 10);
-printf("Igual a 0: %d\n", numero == 0);
-printf("Distinto de 5: %d\n", numero != 5);
-
-if(edad < edad2){
-  printf("la segunda persona es mayor");
-  return 0;
+if(a>b>c){
+  printf("el orden correcto es %d %d %d \n" ,a ,b ,c);
+}else if(b>a>c){
+  printf("el orden correcto es %d %d %d \n" ,b ,a ,c);
+}else if(c>a>b){
+  printf("el orden correcto es %d %d %d \n" ,c ,a ,b);
+}else if(b>c>a){
+  printf("el orden correcto es %d %d %d \n" ,b ,c ,a);
+}else if(c>b>a){
+  printf("el orden correcto es %d %d %d \n" ,c ,b ,a);
+}else if(a>c>b){
+  printf("el orden correcto es %d %d %d \n" ,b ,a ,c);
 }else{
-  printf("la segunda persona es menor");
+  printf("no se que me falto");
 }
 
-  return 0;
+    return 0;
 }

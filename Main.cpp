8a0b4603e440692143cf6a63c7ce8c640559b9d9
Franco Bocanegra <franco.bocanegra@outlook.com>
2026-04-08 21:51:21 -0300

    clase de struct

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 
+
 int main() {
+  int i;
+  int n, s, c;
+  int edad[20];
+  int importe[50];
+  int pasajeros[20];
+  int opcion;
+/*   int totalRecaudacion = 0,
+   cantidadPasajerosNeuquen= 0,
+   cantidadPasajerosSanpedro= 0,
+   cantidadPasajerosCapilla= 0,
+   pasajerosMayores = 0,
+   pasajerosMenores = 0,
+   recaudacionSanpedro= 0,
+   recaudacionNeuquen= 0 ,
+   promedioNeuquen= 0; */
+ 
+   
+          
+          return 0;  
 
 
-}
 
+    }

6bf60d545a206b9667ad8c5d4d7c322984f87563
Franco Bocanegra <franco.bocanegra@outlook.com>
2026-04-14 20:24:00 -0300

    se comenzo con un ejercicio de funciones

--- Main.cpp
+++ Main.cpp
@@ -1,74 +1,99 @@
 #include <stdio.h>
+int puedeIngresar(int edad);
+void mostrarResultado(char nombre[], int edad);
+
+int main() {
+ char nombre[20];
+ int edad;
+ int opcion = 1;
+
+ do{
+    printf("Nombre: ");
+ scanf("%s", &nombre);
+
+ printf("Edad: ");
+ scanf("%d", &edad);
+}
+while (opcion != 1);
+{
+ mostrarResultado(nombre, edad);
+}
+
+ return 0;
+}
+
+int puedeIngresar(int edad) {
+ return edad >= 18;
+}
+void mostrarResultado(char nombre[], int edad) {
+ if (puedeIngresar(edad)) {
+ printf("%s puede ingresar\n", nombre);
+ } else {
+ printf("%s no puede ingresar\n", nombre);
+ }
+}
+
+
+/* #include <stdio.h>
 #include <string.h>
 
 int opcion = 1;
-
-int cantPasajeros= 0; // dato entero
-float totalRecaudado= 0; // dato decimal
-int cantJubilados = 0;
-int cantMenores = 0;
-
-struct Pasajero {
-int edad;
-float precio;
+struct Persona {
+int edad[8];
 char nombre[20];
-char destino[20];
 };
+
+ Persona a;
+ 
+
+int puedeIngresar(int (a.edad));
+int tipoIngreso(int (a.edad));
+int mostrarResultado(char nombre, int edad);
+
 
 int main() {
      
-  Pasajero a;
-  printf("si deseas cargar un pasajero ingrese 1 de lo contrario ingrese 0 para finalizar el programa:");    
+ 
+   printf("si deseas cargar un pasajero ingrese 1 de lo contrario ingrese 0 para finalizar el programa:");    
     scanf("%d", &opcion);
+  
   do{
 
     int ch;
     while ((ch = getchar()) != '\n' && ch != EOF) {}
 
-    printf("Ingrese nombre y apellido: ");
+    printf("Ingrese nombre: ");
     fgets(a.nombre, sizeof(a.nombre), stdin);
     a.nombre[strcspn(a.nombre, "\n")] = '\0';   // quitar el salto de línea
 
-    printf("Ingrese el destino del pasaje, sea 'N' ,'S' o 'D' las opciones disponibles: ");
-    fgets(a.destino, sizeof(a.destino), stdin);
-    a.destino[strcspn(a.destino, "\n")] = '\0';
 
     printf("Ingrese edad: ");
     scanf("%d", &a.edad);
 
-
-    printf("Ingrese precio del pasaje: ");
-    scanf("%f", &a.precio);
-
-    if(a.edad>61){
-      cantJubilados ++;
-    }else if(a.edad<19){
-      cantMenores ++;
-    } 
-
-    cantPasajeros ++;
-    totalRecaudado = totalRecaudado + a.precio;
-
-    printf("\n--- Datos cargados ---\n");
-    printf("Nombre: %s\n", a.nombre);
-    printf("Edad: %d\n", a.edad);
-    printf("Precio pasaje: %.2f\n", a.precio);
-    printf("destino %s\n", a.destino);
-
     printf("si deseas cargar un pasajero ingrese 1 de lo contrario ingrese 0 para finalizar el programa:");    
     scanf("%d", &opcion);
+  
+  
+}while(opcion == 1);
+{
 
-  }while (opcion == 1);
-  {
-    printf("La cantidad total de pasajeros es: %d \n", cantPasajeros);
-    printf("La cantidad total recaudada es: %.2f \n", totalRecaudado);
-    printf("La cantidad total de mayores es: %d \n", cantJubilados);
-    if(a.edad <= 18 && a.precio >= 10000){
-      printf("La cantidad de pasajeros menores que pagaron mas de 10mil es: %d \n", cantMenores);
-    }
-  }  
-          
+}
+
+  
 return 0;  
 
+}
 
-    }
+int tipoIngreso(int (a.edad)){
+    if(a.edad <= 12){
+      return 1;
+    }else if(a.edad>= 12 && a.edad<= 18){
+       return 0;
+        }else{
+            return 2;
+        }
+}
+
+void mostrarResultado{
+
+} */

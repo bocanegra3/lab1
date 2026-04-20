bc4aec2809380b53e2792c1927e655ba90ee4d1c
Franco Bocanegra <franco.bocanegra@outlook.com>
2026-04-20 16:55:57 -0300

    se inicio ejercicio de piedra papel o tijera

--- Main.cpp
+++ Main.cpp
@@ -1,2 +1,44 @@
 #include <stdio.h>
+#include <math.h>
+/* comentarios */
+/* objeto jugadores{
+nombres jugadores
+victoria 
+empate
+derrota
+cantidad de partidas totales
 
+funcion resultado();
+
+main{
+ingresar nombre jugador 1{ }
+ingresar nombre jugador 2 { }
+
+bucle{
+elija piedra papel o tijera jugador 1.
+elija piedra papel o tijera jugador 2.
+
+resultadoParcial()
+}hasta(fin = 1){
+resultadoFinal()
+}
+
+funcion resultadoParcial{
+si(si a+b= 3 y a=1 y b=2){
+imprimir: jugador 1 uso piedra y jugador 2 uso papel, gana el jugador2}
+}si (a+b= 3 y a=1 y b=3){
+imprimir: jugador 1 uso piedra y jugador 2 uso papel, gana el jugador2}
+si (a+b= 3 y a=2 y b=1){
+imprimir: jugador 1 uso piedra y jugador 2 uso papel, gana el jugador2}
+si (a+b= 3 y a=2 y b=3){
+imprimir: jugador 1 uso piedra y jugador 2 uso papel, gana el jugador2}
+si (a+b= 3 y a=3 y b=1){
+imprimir: jugador 1 uso piedra y jugador 2 uso papel, gana el jugador2}
+si (a+b= 3 y a=3 y b=2){
+imprimir: jugador 1 uso piedra y jugador 2 uso papel, gana el jugador2}
+}
+
+ */
+int main(){
+
+}

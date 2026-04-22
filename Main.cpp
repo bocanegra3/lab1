046f4e25fc4fde98dc846fa4b8c96cd1ba2c55f2
Franco Bocanegra <franco.bocanegra@outlook.com>
2026-04-22 16:46:48 -0300

    se incio un proyecto nuevo para ejercitar

--- Main.cpp
+++ Main.cpp
@@ -1,86 +1,54 @@
 #include <stdio.h>
-#include <math.h>
+#include <ctype.h>
 #include <string.h>
 /* comentarios */
-int continuar=0;
-
-struct Jugadores{
-    char nombre[2][20];
-    int victorias= 0;
-    int derrotas= 0;
-    int empates= 0;
-    int partidasTotales= 0;
-    int seleccion;
+/* Control de gimnasio/ */
+/* ingreso de personas para una promo por dia. */
+int importe = 0;
+struct Persona{
+    char nombre[20];
+    int edad;
+    char pase[3]={'S','N','D'};
 }
 
-int resultadoParcial();
-void resultadoFinal();
 
 int main(){
+ Persona lista[100];
+    do{
+        printf("ingrese su nombre: ");
+        fgets(lista[n].nombre, sizeof(lista[n].nombre), stdin);
+        lista[n].nombre[strcspn(lista[n].nombre "\n")] = '\0';
+        printf("Ingrese su edad: ");
 
-    Jugadores a;
-    Jugadores b;
+        scanf("%d\n", &lista[n].edad);
 
-    printf("Ingrese el nombre del Jugador uno: ");
-    fgets(a.nombre, sizeof(a.nombre), stdin);
-    a.nombre[strcspn(a.nombre,"\n")]= '\0';
+        do
+        {
+            printf("ingrese el tipo de pase");
+            scanf("%c \n", &lista[n].pase);
+            if ((lista[n].pase != 'D') && (lista[n].pase != 'S') && (lista[n].pase != 'M'))
+            {
+                printf("Usted no ingreso ninguna de las consonantes validas");
+            }
+        } while ((lista[n].pase != 'D') && (lista[n].pase != 'S') && (lista[n].pase != 'M'));{
+        if (lista[n].pase == 'D')
+        {
+            (importe = importe + 2000);
+        }
+        else if (lista[n].pase == 'S')
+        {
+            (importe = importe + 10000);
+        }
+        else if (lista[n].pase == 'M')
+        {
+            (importe = importe + 35000);
+        }
 
-    printf("Ingrese el nombre del Jugador dos: ");
-    fgets(b.nombre, sizeof(b.nombre), stdin);
-    b.nombre[strcspn(b.nombre, "\n")]= '\0';
-
-do{
-    printf("%s Seleccione 1 para piedra, 2 para papel o 3 para tijera : ", a.nombre);
-    scanf("%d \n", &a.seleccion);
-
-    printf("%s Seleccione 1 para piedra, 2 para papel o 3 para tijera : ", b.nombre);
-    scanf("%d \n", &b.seleccion);
-
-    resultadoParcial();
-
-    a.partidasTotales ++;
-    
-    printf("Si desea seguir Jugando precione 1, de lo contrario preseione 0\n");
-    scanf("%d", &continuar);
-
-}while(continuar==0){
-resultadoFinal();
-}
-
-    return 0;
-}
-int resultadoParcial(){
-    if((a.seleccion==1)&&(b.seleccion==2)){
-        printf("%s a ganado con piedra a %s que utilizo papel", b.nombre, a.nombre);      
-        b.victoria++;  
-        a.derrotas++;
-    }else if((a.seleccion==1)&&(b.seleccion==3)){
-        printf("%s a ganado con piedra a %s que utilizo tijera", a.nombre, b.nombre); 
-        a.victoria++;  
-        b.derrotas++;       
-    }else if((a.seleccion==2)&&(b.seleccion==1)){
-        printf("%s a ganado con papel a %s que utilizo piedra", a.nombre, b.nombre);    
-        a.victoria++;  
-        b.derrotas++;    
-    }else if((a.seleccion==2)&&(b.seleccion==3)){
-        printf("%s a ganado con tijera a %s que utilizo papel", b.nombre, a.nombre);
-        b.victoria++;  
-        a.derrotas++;        
-    }else if((a.seleccion==3)&&(b.seleccion==1)){
-        printf("%s a ganado con piedra a %s que utilizo tijera", b.nombre, a.nombre);  
-        b.victoria++;  
-        a.derrotas++;      
-    }else if((a.seleccion==3)&&(b.seleccion==2)){
-        printf("%s a ganado con tijera a %s que utilizo papel", a.nombre, b.nombre); 
-        a.victoria++;  
-        b.derrotas++;       
-    }else{
-        printf("Los dos jugadores han elegido %d por ende es un empate", a.seleccion);
-        a.empate++;
-        b.empate++;
+        n++;
+    } while (lista[n].nombre == "fin"){
+        printf("el importe total de la semana es %d\n", importe);
     }
 }
 
-void resultadoFinal(){
-    
+    return 0;
 }

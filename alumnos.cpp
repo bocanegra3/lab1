b91b6fd56aa8bfb9aa37d95e4cc53d9189c09569
Franco Bocanegra <franco.bocanegra@outlook.com>
2026-05-03 20:57:14 -0300

    se ordeno

--- alumnos.cpp
+++ alumnos.cpp
@@ -10,16 +10,25 @@ struct Alumnos
 
 Alumnos lista[100];
 
-int i= 0;
+char info[100][10];
+int i = 0;
 
 void cargarAlumnos(){
-printf("Coloque el nombre del alumno: ");
+while (1){
+    printf("Coloque el nombre del alumno: ");
 fgets(lista[i].nombre, sizeof(lista[i].nombre), stdin);
 lista[i].nombre[strcspn(lista[i].nombre, "\n")] = '\0';
+
+strcpy(info[i], lista[i].nombre);  
+
+if (strcmp(lista[i].nombre, "0") == 0) break;
+
 
 printf("Coloque el apellido del alumno: ");
 fgets(lista[i].apellido, sizeof(lista[i].apellido), stdin);
 lista[i].apellido[strcspn(lista[i].apellido, "\n")] = '\0';
+
+strcpy(info[i], lista[i].apellido);  
 
 printf("Coloca la nota del alumno: ");
 scanf("%d", lista[i].nota);
@@ -27,9 +36,11 @@ scanf("%d", lista[i].nota);
 int ch;
 while(((ch = getchar()) != '\n') && ch != EOF){}
 
+i++;}
 }
-void mostrarAlumnos(){
 
+void mostrarAlumnos(){
+printf("%s" , info[i]);
 }
 
 void notaPromedio(){
